3de96c8508931c7653463e5ad6b185f58846ac9a
PromiSe#### <vicimpa@ya.ru>
2020-09-10 19:26:12 +0300

    Компонент информации об игре

---
+++ components/GameInfo/GameInfo.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./GameInfo.sass";
+import { ReactRenderer } from "lib/ReactRenderer";
+
+interface IGameInfoProps {
+  renderer: ReactRenderer
+}
+
+export const GameInfoComponent = ({renderer}: IGameInfoProps) => {
+  const [{ticks, score}] = renderer.useInfo()
+
+  return (
+    <div className="gameinfo-component">
+      <p>Score: <b>{score}</b>, Ticks: {ticks}</p>
+    </div>
+  )
+}
